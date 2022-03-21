4212b910f87d8e31b6c9ef3c3ed822614838a828
Jhon Nicolas Fajardo Jerez <jfajardoj@unal.edu.co>
2022-03-20 20:33:41 -0500

    añadido uso de variables como límites

--- tarea01.cpp
+++ tarea01.cpp
@@ -1,14 +1,19 @@
+//Este programa suma los inversos de los numeros pares entre dos numeros "start" y "end" 
+
 #include <iostream>
 
 int main()
 {
     int n{};
+    int start{10};
+    int end{100};
     double result{};
-    for(n=10; n<=100 and n>=10 ;n=n+1)
+    for(n=start; n>=start and n<=end ;n=n+1)
     {
         if(n%2 == 1)
         {
             result = result + 1.0/n;
+            std::cout << n <<" - "<< result << "\n";
         }  
     }
     std::cout << result << "\n";
